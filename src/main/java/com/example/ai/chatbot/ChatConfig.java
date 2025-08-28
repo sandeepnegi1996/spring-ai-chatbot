269b3bdcff84915f4bf998ffaac6a852e428fcbf
sandy <9041128179sandy@gmail.com>
2025-08-28 23:37:09 +0530

    commented the chat client code

--- src/main/java/com/example/ai/chatbot/ChatConfig.java
+++ src/main/java/com/example/ai/chatbot/ChatConfig.java
@@ -1,17 +1,17 @@
-package com.example.ai.chatbot;
-
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ChatConfig {
-
-
-    @Bean("openAiChatClient")
-  public ChatClient chatClient(ChatClient.Builder builder) {
-
-        return builder.defaultSystem(" You are a helpful and formal assistant Keep answers concise unless asked for detail.").build();
-    }
-
-}
+//package com.example.ai.chatbot;
+//
+//import org.springframework.ai.chat.client.ChatClient;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ChatConfig {
+//
+//
+//    @Bean("openAiChatClient")
+//  public ChatClient chatClient(ChatClient.Builder builder) {
+//
+//        return builder.defaultSystem(" You are a helpful and formal assistant Keep answers concise unless asked for detail.").build();
+//    }
+//
+//}
